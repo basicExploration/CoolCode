91fee0c84ad43645e24b886ec960af2a51c27ce0
googege <googege@icloud.com>
2019-08-31 21:12:55 +0800

    add

--- 1/1/1.go
+++ 1/1/1.go
@@ -1,1 +1,33 @@
 package main
+
+import "math"
+
+func ThreeSum(nums []int) [][]int {
+	if len(nums) < 3 {
+		return nil
+	}
+	value := make(map[float64]int)
+	result := make([][]int, 0)       // 初始化数据结构
+	for i := 0; i < len(nums); i++ { // 三层数据遍历
+		for j := i + 1; j < len(nums); j++ {
+			for q := j + 1; q < len(nums); q++ {
+				if nums[i]+nums[j]+nums[q] == 0 { //判断数据是否等于0
+					resultN := make([]int, 3)
+					resultN[0], resultN[1], resultN[2] = nums[i], nums[j], nums[q]
+					if Pan(math.Abs(float64(resultN[0]))+math.Abs(float64(resultN[1]))+math.Abs(float64(resultN[2])), value) {
+						result = append(result, resultN) // 将数据合成
+					}
+				}
+			}
+		}
+	}
+	return result
+}
+
+func Pan(v float64, value map[float64]int) bool {
+	if _, ok := value[v]; ok {
+		return false
+	}
+	value[v]++
+	return true
+}
--- 1/1/1_test.go
+++ 1/1/1_test.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestThreeSum(t *testing.T) {
+	fmt.Println("正常测试结果", ThreeSum([]int{1, -1, 0, 2, 4, -2}))
+	fmt.Println("返回nil的结果", ThreeSum([]int{1, 2}))
+	fmt.Println("返回空的结果", ThreeSum([]int{1, 1, 1, 1, 1}))
+}
